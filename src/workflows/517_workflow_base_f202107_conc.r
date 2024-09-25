84e758e4da7220a5203b3fde33a82dd5c6a95103
lucasreinosomcd <lucas.reinoso.mcd@gmail.com>
2024-09-25 01:00:37 +0000

    auto commit 8       20240925 010033

--- src/workflows/517_workflow_base_f202107_conc.r
+++ src/workflows/517_workflow_base_f202107_conc.r
@@ -151,7 +151,7 @@ FEhist_base <- function( pinputexps)
   param_local$Tendencias1$run <- TRUE  # FALSE, no corre nada de lo que sigue
   param_local$Tendencias1$ventana <- 6
   param_local$Tendencias1$tendencia <- TRUE
-  param_local$Tendencias1$minimo <- FALSE
+  param_local$Tendencias1$minimo <- TRUE
   param_local$Tendencias1$maximo <- FALSE
   param_local$Tendencias1$promedio <- FALSE
   param_local$Tendencias1$ratioavg <- FALSE
