f7c82df5f38f46bdd64245a8d3d399c4ad294e6b
lucasreinosomcd <lucas.reinoso.mcd@gmail.com>
2024-10-04 22:46:09 +0000

    auto commit 32       20241004 224530

--- src/workflows/501_run_workflow_conceptual.r
+++ src/workflows/501_run_workflow_conceptual.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/519_workflow_base_f202109_conc_v10.r"
+PARAM <- "src/workflows/519_workflow_base_f202109_conc_v11.r"
 envg <- env()
 
 envg$EXPENV <- list()
